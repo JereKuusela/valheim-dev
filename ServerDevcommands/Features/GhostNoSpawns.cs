550a320b0aef86380fc393cac9f10dc7d4bbb8ae
Jere Kuusela <jere.kuusela@iki.fi>
2023-02-26 12:26:42 +0200

    Changes the ghost mode to prevent spawns (only when your client is controlling the area).

---
+++ ServerDevcommands/Features/GhostNoSpawns.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using HarmonyLib;
+using UnityEngine;
+
+namespace ServerDevcommands;
+
+[HarmonyPatch]
+public class GhostIgnorePlayers
+{
+  private static List<Player> Players()
+  {
+    return Player.m_players.Where(p => !p.InGhostMode()).ToList();
+  }
+
+  static IEnumerable<CodeInstruction> ReplacePlayers(IEnumerable<CodeInstruction> instructions)
+  {
+    return new CodeMatcher(instructions)
+         .MatchForward(
+             useEnd: false,
+             new CodeMatch(OpCodes.Ldsfld, AccessTools.Field(typeof(Player), nameof(Player.m_players))))
+         .SetAndAdvance(
+              OpCodes.Call, Transpilers.EmitDelegate(Players).operand)
+         .InstructionEnumeration();
+  }
+  [HarmonyPatch(typeof(SpawnSystem), nameof(SpawnSystem.GetPlayersInZone)), HarmonyPostfix]
+  static void GhostNoSpawns(List<Player> players)
+  {
+    var toRemove = players.Where(p => p.InGhostMode()).ToList();
+    foreach (var player in toRemove)
+      players.Remove(player);
+  }
+  [HarmonyPatch(typeof(Player), nameof(Player.GetClosestPlayer)), HarmonyTranspiler]
+  static IEnumerable<CodeInstruction> GetClosestPlayer(IEnumerable<CodeInstruction> i) => ReplacePlayers(i);
+
+  [HarmonyPatch(typeof(Player), nameof(Player.IsPlayerInRange), typeof(Vector3), typeof(float)), HarmonyTranspiler]
+  static IEnumerable<CodeInstruction> IsPlayerInRange(IEnumerable<CodeInstruction> i) => ReplacePlayers(i);
+
+  [HarmonyPatch(typeof(Player), nameof(Player.IsPlayerInRange), typeof(Vector3), typeof(float), typeof(float)), HarmonyTranspiler]
+  static IEnumerable<CodeInstruction> IsPlayerInRange2(IEnumerable<CodeInstruction> i) => ReplacePlayers(i);
+
+}
--- publish/README.md
+++ publish/README.md
@@ -25,6 +25,7 @@ Donations: [Buy me a computer](https://www.buymeacoffee.com/jerekuusela)
 - v1.42
   - Internal change to support Infinity Hammer mod.
   - Changes the command `resetskill` to reset all skills with the `All` argument (to match `raiseskill` behavior).
+  - Changes the ghost mode to prevent spawns (only when your client is controlling the area).
   - Fixes the `search_id` command.
   - Fixes the "No weight limit with god mode" setting appearing to work on other players (with mods that allow inspecting other players).
   - Removes the customized `raiseskill` command (vanilla supports now the `All` argument).
